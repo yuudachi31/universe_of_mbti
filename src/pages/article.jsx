53d4ba894e14aa00d25471df9fcd34791fc0527b
yuudachi31 <43200967+yuudachi31@users.noreply.github.com>
2022-10-31 00:51:15 +0800

    add:article-detail

--- src/pages/article.jsx
+++ src/pages/article.jsx
@@ -52,8 +52,20 @@ const ArticleView = () => {
 
 <hr className="article-detail-bottomline" Size="2px"  width="100%"></hr>
 <div className="article-detail-link-cont">
-  
+  <div className="copy-link-subcont">
+  <div className="copy-link">點我複製連結</div>
+  <img src="/images/article_link.png"></img>
+  </div>
 </div>
+<div className="change-article-bigcont">
+<div className="change-article-title">閱讀下一篇：</div>
+<div className="change-article-subcont">
+   <ChangeArticleCard/>
+<ChangeArticleCard/>
+</div>
+ 
+</div>
+
 </div>
         <Footer/>
       </div>
@@ -61,5 +73,22 @@ const ArticleView = () => {
   );
 };
 
+const ChangeArticleCard = ({ item }) => {
+ 
+  return (
+    <Link className="link" to="/article/id">
+     <div className="change-articleCard-cont">
+     
+      <div className="articleCard-textblock">
+        <div className="articleCard-title">    {articleData.articleTitle}</div>
+        <div className="change-articleCard-description">{articleData.articleDescription}</div>
+     
+      </div>
+    
+    </div>
+    </Link>
+   
+  );
+};
 
 export default ArticleView;
