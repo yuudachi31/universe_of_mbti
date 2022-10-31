9f2bc2ffa33db0137e5609b2021f47dee60e5352
Albada16 <NamelessOAO@gmail.com>
2022-10-31 17:52:20 +0800

    banner大改版

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Selectadmin, articleEditing } from "../redux/adminSlice";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 import homeData from "../json/home.json";
+import Banner from "../components/Banner";
 import HomeSection from "../components/HomeSection";
 // import {} from "../redux/adminSlice"
 const HomeView = () => {
@@ -25,26 +26,9 @@ const HomeView = () => {
   //   console.log(admin);
   return (
     <>
-      <Navbar posr={true}></Navbar>
+      <Navbar ></Navbar>
       <div className="home-container">
-        <div className="home-top">
-          <div className="home-top-newcont">
-            <div className="homeValue">
-              <img className="homeTitle" src="./images/Frame.png"></img>
-              <div className="homeSlogan homeSlogan-text">透過MBTI來認識</div>
-              <div className="homeSlogan homeSlogan-text">誕生於宇宙中</div>
-              <div className="homeSlogan homeSlogan-text">獨一無二的自己</div>
-              <Link className="go-btn homeSlogan-text" to="/worldView">
-                世界觀 GO!
-              </Link>
-            </div>
-            <img className="homebg1" src="./images/cover.png"></img>
-
-          </div>
-
-          {/* <img className="down-btn" src="./images/downBtn.png"></img> */}
-        </div>
-
+        <Banner></Banner>
         <div className="home-content">
           {homeData.map((item) => {
             return <HomeSection item={item}></HomeSection>;
@@ -56,3 +40,4 @@ const HomeView = () => {
 };
 
 export default HomeView;
+
