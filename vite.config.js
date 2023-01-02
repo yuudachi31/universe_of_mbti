565ce79d7c828cfa5aa0a7c4a5baf6371147f73d
yuudachi31 <43200967+yuudachi31@users.noreply.github.com>
2023-01-03 01:44:12 +0800

    addvite

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import vitePluginImp from 'vite-plugin-imp'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  esbuild: {
+    loader: 'jsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  plugins: [
+    react(),
+    vitePluginImp({
+      optimize: true,
+      libList: [
+        {
+          libName: 'antd',
+          libDirectory: 'es',
+          style: (name) => `antd/es/${name}/style`
+        }
+      ]
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true, 
+      }
+    }
+  },
+})
